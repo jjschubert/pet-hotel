a31c271841e8b7e2c06e775ce0eec53e2f6f86c9
rileyww77 <riley_ww77@yahoo.com>
2020-09-24 10:05:35 -0500

    basic setup for index.js done

--- src/index.js
+++ src/index.js
@@ -3,15 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
 import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Create the rootSaga generator function
+function* rootSaga() {
+
+}
+
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+const storeInstance = createStore(
+  combineReducers({
+  
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render( <Provider store={storeInstance}> <App /></Provider>,
+  document.getElementById('root')
+);
 serviceWorker.unregister();
