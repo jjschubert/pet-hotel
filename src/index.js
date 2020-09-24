aea825aa87ddd57eafd0df14d88af350cd8314df
rileyww77 <riley_ww77@yahoo.com>
2020-09-24 10:24:12 -0500

    wrote all sagas and reducers in index. still need to connect them to coresspponding componenets

--- src/index.js
+++ src/index.js
@@ -8,10 +8,92 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+//sagas
+function* fetchPets() {
+  try {
+    let response = yield axios.get('/pets')
+    yield put ({ type: 'SET_PETS', payload: response.data})
+
+  } catch (error) {
+    console.log('error in fetchPets', error)
+  }
+}
+
+function* fetchOwners(){
+  try {
+    let response = yield axios.get('/owners')
+    yield put({type: 'SET_OWNERS', payload: response.data})
+  } catch (error) {
+    console.log('error in fetchOwners', error)
+  }
+}
+
+function* addPet(action) {
+  try {
+    yield axios.post('/pets', action.payload)
+    yield put({type: 'FETCH_PETS'})
+  } catch (error) {
+    console.log('error in addPet', error)
+  }
+}
+
+function* addOwner(action) {
+  try {
+      yield axios.post('/owners', action.payload)
+      yield put ({type: 'FETCH_OWNERS'})
+  } catch (error) {
+    console.log('error in addOwner', error)
+  }
+}
+
+function* deletePet(action){
+  try{
+    yield axios.delete(`/pets/${action.payload}`)
+    yield put ({ type: 'FETCH_PETS' })
+  } catch (error) {
+    console.log('error in deletePet', error)
+  }
+}
+
+function* deleteOwner(action) {
+  try{
+    yield axios.delete(`/owners/${action.payload}`)
+    yield put ({type: 'FETCH_OWNERS'})
+  } catch(error) {
+    console.log('error in deleteOwner', error)
+  }
+}
+// reducers
+const petsReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_PETS':
+      return action.payload
+    default:
+      return state
+  }
+}
+
+const ownerReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_OWNERS':
+      return action.payload
+    default:
+      return state
+  }
+}
+
+
 
 // Create the rootSaga generator function
 function* rootSaga() {
-
+    yield takeEvery('FETCH_PETS', fetchPets)
+    yield takeEvery('FETCH_OWNERS', fetchOwners)
+    yield takeEvery('ADD_PET', addPet)
+    yield takeEvery('ADD_OWNER', addOwner)
+    yield takeEvery('DELETE_PET', deletePet)
+    yield takeEvery('DELETE_OWNER', deleteOwner)
 }
 
 // Create sagaMiddleware
@@ -19,7 +101,8 @@ const sagaMiddleware = createSagaMiddleware();
 
 const storeInstance = createStore(
   combineReducers({
-  
+    petsReducer,
+    ownerReducer
   }),
   // Add sagaMiddleware to our store
   applyMiddleware(sagaMiddleware, logger),
